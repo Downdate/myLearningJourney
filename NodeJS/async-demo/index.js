d9ddf591953f20e6e444f433e04e8596379053b0
Daniel Zarei <danielZareiMain@gmail.com>
2023-06-19 18:14:00 +0430

    new promise file added

--- NodeJS/async-demo/index.js
+++ NodeJS/async-demo/index.js
@@ -1,7 +1,26 @@
 console.log('before')
+//get the user
+getuser(1, (user) => {
+    console.log(`user: `, user)
 
-setTimeout(()=>{
-    console.log('reading a user from the database!')
-},2000)
+    // get the repositories
+    getRepositories(user.githubUsername,(userRepo) => {
+        console.log('Repositories: ',userRepo)
+    })
+})
 
 console.log('after')
+
+function getuser(id, callback) {
+    console.log('reading a user from database!')
+    setTimeout(()=>{
+        callback({id: id, githubUsername: `Daniel`})
+    },2000)
+}
+
+function getRepositories (username, callback) {
+    setTimeout(() => {
+        console.log(`getting ${username}'s repositories from database ....`)
+        callback(['repo1','repo2','repo3'])
+    }, 2000)
+}
