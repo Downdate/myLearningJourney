f7d3d6660e2b21e225edb143f866e1290f32df33
Daniel Zarei <danielZareiMain@gmail.com>
2023-06-22 21:36:44 +0430

    callback hell changed to promises

--- NodeJS/async-demo/index.js
+++ NodeJS/async-demo/index.js
@@ -1,26 +1,41 @@
 console.log('before')
 //get the user
-getuser(1, (user) => {
-    console.log(`user: `, user)
-
-    // get the repositories
-    getRepositories(user.githubUsername,(userRepo) => {
-        console.log('Repositories: ',userRepo)
+getuser(1)
+    .then(user => {
+        getRepositories(user.githubUsername)
+            .then(repos => {
+                getCommits(repos[0])
+                    .then(commits => {
+                        console.log('commits: ',commits)
+                    })
+            })
     })
-})
 
 console.log('after')
 
-function getuser(id, callback) {
-    console.log('reading a user from database!')
-    setTimeout(()=>{
-        callback({id: id, githubUsername: `Daniel`})
-    },2000)
+function getuser(id) {
+    return new Promise((resolve,reject) => {
+        console.log('reading a user from database!')
+        setTimeout(()=>{
+            resolve({id: id, githubUsername: `Daniel`})
+        },2000)
+    })
 }
 
-function getRepositories (username, callback) {
-    setTimeout(() => {
-        console.log(`getting ${username}'s repositories from database ....`)
-        callback(['repo1','repo2','repo3'])
-    }, 2000)
+function getRepositories (username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`getting ${username}'s repositories from database ....`)
+            resolve(['repo1','repo2','repo3'])
+        }, 2000)
+    })
+}
+function getCommits(repository) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log(`getting the commits for ${repository} ....`)
+            resolve([`commit1`,`commit2`,`commit3`])
+        },2000)
+    })
+
 }
