fc93fadd8fe1c8c97bdee0f9e97f61fc02db4c8a
Daniel Zarei <danielZareiMain@gmail.com>
2023-07-30 17:34:56 +0430

    database operation added to mongodb-demo

--- NodeJS/mongodb-demo/index.js
+++ NodeJS/mongodb-demo/index.js
@@ -1,6 +1,100 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost:27017/playground')
     .then(() => console.log('connected to mongodb...') )
     .catch(err => console.error('Could not connect to mongodb...',err))
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean,
+    price: Number
+})
+
+const Course = mongoose.model('course', courseSchema)
+
+async function createCourse(){
+    const courseNo1 = new Course({
+        name: 'node.js Course',
+        author: 'Dan',
+        tags: ['node','backend'],
+        isPublished: true,
+        price: 10
+    })
+
+    const result = await courseNo1.save()
+
+    console.log(result)
+}
+
+async function getCourses(){
+    const pageNumber = 2
+    const pageSize = 10
+    const courses = await Course
+        // exact filters
+        //.find({author: 'Dan', isPublished: true , price: { $gte: 10}})
+
+        // logical filters
+        //.find()
+        //.or([{author: 'Alex'},{isPublished:true}])
+
+        //starts with Dan
+        .find({author: /^Dan/i})
+
+        //ends with Dan
+        //.find({author: /Dan$/i})
+
+        //contains Dan
+        //.find({author: /.*Dan.*/i})
+
+        //.skip((pageNumber-1)*pageSize)
+
+        .limit(10)
+        .sort({name: 1 })
+        //.count()
+        .select({
+            name: 1 , tags: 1
+        })
+    console.log(courses)
+}
+
+async function updateCourseRet(id){
+    // query first then update approach
+    const course = await Course.findById(id)
+    if (!course) return
+    course.isPublished = true
+    course.author = 'Another Author'
+    //set method for updating
+    //course.set({
+    //    isPublished: true,
+    //    author: 'Another Author'
+    //})
+    const result = await course.save()
+    console.log(result)
+}
+
+async function updateCourse(id){
+    // direct update approach
+    const result = await Course.findByIdAndUpdate({_id: id},{
+        $set:{
+            author: 'Alex',
+            isPublished: false
+        }
+    },{new: true})
+    console.log(result)
+}
+
+async function removeCourse(id){
+    //const result = await Course.deleteOne({ _id : id})
+    const course = await Course.findByIdAndRemove(id)
+    //console.log(result)
+    console.log(course)
+}
+
+//getCourses()
+//createCourse()
+//updateCourseRet('64c63f92f998ce3420c762f6')
+//updateCourse('64c63f92f998ce3420c762f6')
+//removeCourse('64c63f92f998ce3420c762f6')
